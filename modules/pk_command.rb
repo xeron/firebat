2a8ba6e39708204b24c5aa3f93a48db654c4bb7e
Ivan Larionov <xeron.oskom@gmail.com>
2016-10-02 01:40:10 -0700

    Fix pk moduke + rubocop.

--- modules/pk_command.rb
+++ modules/pk_command.rb
@@ -1,68 +1,69 @@
-# coding: utf-8
-
 class PkCommand < FireBatCommand
   require_roles :oper
 
   def on_privmsg(cmd)
     if cmd.args(1, 1)
-      msg = case cmd.args(1, 1)
-      when "+" then add(cmd.args(1, 2), cmd.args_tail(1, 3), cmd.nick)
-      when "-" then rm(cmd.args_tail(1, 2))
-      when "help" then help(cmd)
-      else info(cmd.args_tail(1, 1))
-      end
+      msg =
+        case cmd.args(1, 1)
+        when '+' then add(cmd.args(1, 2), cmd.args_tail(1, 3), cmd.nick)
+        when '-' then rm(cmd.args_tail(1, 2))
+        when 'help' then help(cmd)
+        else info(cmd.args_tail(1, 1))
+        end
     else
       msg = list
     end
+
     @irc.privmsg cmd.reply, msg
   end
 
   def list
-    msg = "Найдено: [" + Pk.find(:all, :order => "name").collect(&:name).join(", ") + "]"
+    'Найдено: [' + Pk.all.order(:name).collect(&:name).join(', ') + ']'
   end
 
   def info(name)
-    name = "%#{name}%"
-    if p = Pk.find(:first, :conditions => ["name like ?", name])
-      "Найдено: #{p.name}. Добавил: #{p.by}\nПричина: #{p.reason}"
+    if (pk = Pk.where('name like ?', "%#{name}%").first)
+      "Найдено: #{pk.name}. Добавил: #{pk.by}\nПричина: #{pk.reason}"
     else
-      "Не найдено (#{name})"
+      "Не найдено: #{name}"
     end
   end
 
   def add(name, reason, nick)
-    if p = Pk.find_by_name(name)
-      p.reason = reason
-      p.save
-      "Причина для #{p.name} обновлена"
+    if (pk = Pk.find_by_name(name))
+      pk.reason = reason
+      pk.save
+      "Причина для #{name} обновлена"
     else
-      p = Pk.create(:name => name, :reason => reason, :by => nick)
-      "#{p.name} добавлен в список жертв!"
+      Pk.create(name: name, reason: reason, by: nick)
+      "#{name} добавлен в список жертв!"
     end
   end
 
   def rm(name)
-    if p = Pk.find_by_name(name)
+    if (p = Pk.find_by_name(name))
       p.destroy
       "#{name} удален из списка"
     else
-      "Не найдено (#{name})"
+      "Не найдено: #{name}"
     end
   end
 
   def help(cmd)
-    msg = "Модуль контроля пк-листа для RMUD IRC Bot. Автор: Xeron. Синтаксис: !пк action <params>.
-Actions:
-!пк => Показывает пк-лист
-!пк <имя> => Показывает персонажа и причину
-!пк + <имя> <причина> => Добавляет в список, либо обновляет причину
-!пк - <имя> => Удаляет из списка"
+    msg = <<~EOS
+      Модуль контроля пк-листа для RMUD IRC Bot. Автор: Xeron. Синтаксис: !пк action <params>.
+      Actions:
+      !пк => Показывает пк-лист
+      !пк <имя> => Показывает персонажа и причину
+      !пк + <имя> <причина> => Добавляет в список, либо обновляет причину
+      !пк - <имя> => Удаляет из списка
+    EOS
     reply cmd.nick, msg
-    msg = ""
+    false
   end
 
   def privmsg_filter(cmd)
-    cmd.args(1, 0) == "!пк"
+    cmd.args(1, 0) == '!пк'
   end
 
   class Pk < ActiveRecord::Base
@@ -71,18 +72,15 @@ Actions:
   class Pk::Install < ActiveRecord::Migration
     def self.up
       create_table :pks do |t|
-        t.column :by, :string, :limit => 30
-        t.column :name, :string, :limit => 100
+        t.column :by, :string, limit: 30
+        t.column :name, :string, limit: 100
         t.column :reason, :text
       end
-      add_index :pks, :name, :unique => true
+      add_index :pks, :name, unique: true
     end
   end
 
   def self.install
-    unless Pk.table_exists?
-      Pk::Install.migrate :up
-    end
+    Pk::Install.migrate :up unless Pk.table_exists?
   end
-
 end
