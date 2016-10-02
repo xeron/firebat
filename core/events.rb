4cafbe26c39abec964b199f1d087a35307a8b271
Ivan Larionov <xeron.oskom@gmail.com>
2016-10-02 13:47:19 -0700

    [core/events] rubocop cleanup.

--- core/events.rb
+++ core/events.rb
@@ -1,9 +1,7 @@
 module FireBat
   # Events collector
   # It collects handlers for events and can eval them
-  #
   class Events
-
     attr_reader :events
 
     def initialize
@@ -13,21 +11,17 @@ module FireBat
     # Add command handler as object (class inherits FireBatCommand)
     # Ignore if handler already exists
     # Name arg is command handler name (ex: privmsg, notice)
-    #
     def add(name, obj)
-      if @events[name]
-        unless @events[name].include?(obj)
-          @events[name] << obj
-        end
+      if @events[name] && !@events[name].include?(obj)
+        @events[name] << obj
       else
         @events[name] = [obj]
       end
     end
 
     # Remove object from events
-    #
     def delete(obj)
-      for lists in @events
+      @events.each do |lists|
         lists.delete(obj)
       end
     end
@@ -37,51 +31,40 @@ module FireBat
     end
 
     # Scan commands for commands supports cmd.code
-    # and envoke on_*cmd.code* method to every command, passed filter
-    #
+    # and invoke on_*cmd.code* method to every command passed filter
     def parse(cmd)
       return unless @events[cmd.code]
 
-      for command in @events[cmd.code]
-        filter_passed =
-        if command.respond_to?(cmd.code + "_filter")
-          begin
-            command.send((cmd.code + "_filter").to_sym, cmd)
-          rescue => ex
-            puts "Error in filter! #{ex}"
-            print ex.backtrace.join("\n") # print returns nil
-          end
-        else
-          true
-        end
+      @events[cmd.code].each do |command|
+        next unless cmd_filter(cmd, command)
 
-        if filter_passed
-          puts "#{cmd.code} [#{cmd.src}] raises event in #{command.class}"
+        puts "#{cmd.code} [#{cmd.src}] raises event in #{command.class}"
 
-          filter_passed =
-          if command.respond_to?("post_filter")
-            begin
-              command.post_filter(cmd)
-            rescue => ex
-              puts "Error in post-filter! #{ex}"
-              print ex.backtrace.join("\n")
-            end
-          else
-            true
-          end
-        end
+        next unless cmd_filter(cmd, command, 'post_filter')
 
-        if filter_passed
-          puts "post-filter passed"
-          begin
-            command.send(("on_" + cmd.code).to_sym, cmd)
-          rescue => ex
-            puts "Error! #{ex}"
-            print ex.backtrace.join("\n")
-          end
+        puts 'post-filter passed'
+
+        begin
+          command.send(('on_' + cmd.code).to_sym, cmd)
+        rescue => ex
+          puts "Error! #{ex}"
+          print ex.backtrace.join("\n")
         end
       end
     end
 
+    def cmd_filter(cmd, command, filter = nil)
+      filter ||= cmd.code + '_filter'
+      if command.respond_to?(filter)
+        begin
+          command.send(filter.to_sym, cmd)
+        rescue => ex
+          puts "Error in filter! #{ex}"
+          print ex.backtrace.join("\n") # print returns nil
+        end
+      else
+        true
+      end
+    end
   end
 end
