34814be3071b5d115d1cf0dec040224be4c5f476
Ivan Larionov <xeron.oskom@gmail.com>
2016-10-02 13:00:30 -0700

    [core/encoding] rubocop and cleanup.

--- core/encoding.rb
+++ core/encoding.rb
@@ -1,32 +1,27 @@
 module FireBat
   class Encoding
-
     attr_reader :script, :server, :terminal
 
     def initialize(irc_charset)
       @server = irc_charset
-      ENV["LANG"].match(/\.(.*)$/)
-      @terminal = $1.downcase
+      ENV['LANG'] =~ /\.(.*)$/
+      @terminal = Regexp.last_match[1].downcase
       @script = 'utf-8'
 
-      @t_server = @f_server = (script == server) ? false : true
-      @t_std = @f_std = (script == terminal) ? false : true
+      @t_server = @f_server = script != server
+      @t_std = @f_std = script != terminal
     end
 
     def t_s(str)
-      begin
-        @t_server ? str.encode!(server, script) : str.force_encoding(server)
-      rescue => ex
-        puts "Encode failed from bot to server."
-      end
+      @t_server ? str.encode!(server, script) : str.force_encoding(server)
+    rescue
+      puts 'Encode failed from bot to server.'
     end
 
     def f_s(str)
-      begin
-        @f_server ? str.encode!(script, server) : str.force_encoding(server)
-      rescue => ex
-        puts "Encode failed from server to bot."
-      end
+      @f_server ? str.encode!(script, server) : str.force_encoding(server)
+    rescue
+      puts 'Encode failed from server to bot.'
     end
 
     def t_t(str)
@@ -36,6 +31,5 @@ module FireBat
     def f_t(str)
       @f_std ? str.encode!(script, terminal) : str.force_encoding(server)
     end
-
   end
 end
